acd7984b9f58adf123fb184893e2cfb5eeef8158
Thiago Kachinsky <thiagokachidev@gmail.com>
2022-08-23 18:30:40 -0300

    add performance resources

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import { Button } from "./Button";
 
 interface SideBarProps {
@@ -10,7 +11,8 @@ interface SideBarProps {
   buttonClickCallback: (args: any) => void;
 }
 
-export function SideBar({
+// export function SideBar({
+  function SideBarComponent({
   genres,
   selectedGenreId,
   buttonClickCallback
@@ -34,3 +36,5 @@ export function SideBar({
     </nav>
   )
 }
+
+export const SideBar = memo(SideBarComponent)
